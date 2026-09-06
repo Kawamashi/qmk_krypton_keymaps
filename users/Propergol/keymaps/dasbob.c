3e85dda8d036a1a8156328329a24cab6561a3f19
Kawamashi <kawamashi@free.fr>
2026-09-06 17:14:15 +0200

    comma, hyphen inversion

--- users/Propergol/keymaps/dasbob.c
+++ users/Propergol/keymaps/dasbob.c
@@ -23,7 +23,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_split_3x5_3(
-      PG_VIRG, PG_EACU, PG_U,    PG_P,    PG_TIRE,                 PG_V,   PG_M,    PG_C,    PG_J,    PG_X,
+      PG_TIRE, PG_EACU, PG_U,    PG_P,    PG_VIRG,                 PG_V,   PG_M,    PG_C,    PG_J,    PG_X,
       P(PG_O), R(PG_A), M(PG_I), I(PG_N), PG_POIN,                 PG_G,   I(PG_T), M(PG_S), R(PG_R), P(PG_L),
       PG_Q,    PG_EGRV, PG_Y,    PG_H,    PG_B,                    PG_K,   PG_D,    PG_F,    PG_W,    OS_1DK,
                                  OS_SHFT, LT_E,   LT_MGC, LT_REPT, LT_SPC, OS_RTHB
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_1DK] = LAYOUT_split_3x5_3(
-      _______, _______, _______, _______, PG_X,                      _______, _______, _______, _______, _______,
+      PG_X,    PG_VIRG, _______, _______, _______,                   _______, _______, _______, _______, _______,
       _______, _______, _______, PG_Z,    _______,                   _______, _______, _______, _______, _______,
       _______, PG_J,    _______, _______, _______,                   _______, _______, _______, _______, OS_1DK,
                                  _______, PG_ECIR, LT_AGRV, PG_APOS, PG_UNDS, OS_NUM
@@ -41,21 +41,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT_split_3x5_3(
 #if defined KRYPTON_NUMBER_ROW
       // Numbers in row, triggered by a layer-tap key on the left half.
-      PG_VIRG, REV_TAB, ALT_TAB, OS_WNUM, PG_PERC,                   PG_EXP,  KC_PDOT, KC_5,    PG_EURO, PG_DEG,
+      PG_PERC, REV_TAB, ALT_TAB, OS_WNUM, PG_VIRG,                   PG_EXP,  KC_PDOT, KC_5,    PG_EURO, PG_DEG,
       P(KC_6), R(KC_7), M(KC_8), I(KC_9), PG_POIN,                   NUMWORD, I(KC_1), M(KC_2), R(KC_3), P(KC_4),
       PG_PLUS, PG_ASTX, PG_SLSH, PG_MOIN, TG_NUM,                    PG_IND,  KC_ENT,  _______, _______, OS_1DK,
                                  OS_SHFT, LT_SPC,  NNB_SPC, KC_BSPC, LT_0,    OS_SYMB
 
 #elif defined KRYPTON_NUMBER_ROW_WITH_ONESHOT
       // Numbers in row, triggered by a one shot key on the right half.
-      PG_VIRG, _______, KC_5,    KC_PDOT, PG_PERC,                   PG_EXP,  _______, PG_DEG,  PG_EURO, OS_WNUM,
+      PG_PERC, _______, KC_5,    KC_PDOT, PG_VIRG,                   PG_EXP,  _______, PG_DEG,  PG_EURO, OS_WNUM,
       P(KC_6), R(KC_7), M(KC_8), I(KC_9), PG_POIN,                   NUMWORD, I(KC_1), M(KC_2), R(KC_3), P(KC_4),
       PG_PLUS, PG_ASTX, PG_SLSH, PG_MOIN, TG_NUM,                    PG_IND,  KC_ENT,  _______, _______, OS_1DK,
                                  OS_SYMB, LT_0,    KC_BSPC, NNB_SPC, LT_SPC,  OS_NUM
 
 #else
       // Default configuration: numpad
-      PG_VIRG,    REV_TAB,    ALT_TAB,    OS_WNUM,    PG_PERC,                   PG_EXP,  KC_7,    KC_8,    KC_9,    PG_EURO,
+      PG_PERC,    REV_TAB,    ALT_TAB,    OS_WNUM,    PG_VIRG,                   PG_EXP,  KC_7,    KC_8,    KC_9,    PG_EURO,
       P(PG_PLUS), R(PG_ASTX), M(PG_SLSH), I(PG_MOIN), PG_POIN,                   NUMWORD, I(KC_4), M(KC_5), R(KC_6), P(KC_PDOT),
       PG_DEG,     _______,    _______,    KC_ENT,     TG_NUM,                    PG_IND,  KC_1,    KC_2,    KC_3,    OS_1DK,
                                           OS_SHFT,    LT_SPC,  NNB_SPC, KC_BSPC, LT_0,    OS_SYMB
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #else
       // Default configuration: Propergol optimized symbol layer
       // NNB_SPC finishes a numword (e.g. `127 km`) with reversed numrow.
-      ALGR(PG_O), PG_INF,  PG_SUP,  PG_MOIN, PG_PERC,                  PG_ACIR, PG_LCBR, PG_RCBR, PG_DLR,  PG_GRV,
-      PG_PLUS,    PG_ASTX, PG_SLSH, PG_EGAL, PG_BSLS,                  PG_HASH, PG_LPRN, PG_RPRN, PG_PVIR, PG_DQUO,
+      PG_PERC,    PG_INF,  PG_SUP,  PG_MOIN, PG_BSLS,                  PG_ACIR, PG_LCBR, PG_RCBR, PG_DLR,  PG_GRV,
+      PG_PLUS,    PG_ASTX, PG_SLSH, PG_EGAL, PG_POIN,                  PG_HASH, PG_LPRN, PG_RPRN, PG_PVIR, PG_DQUO,
       ALGR(PG_J), PG_TILD, PG_LSBR, PG_RSBR, PG_AROB,                  _______, PG_2PTS, PG_ESPR, PG_PIPE, PG_APOD,
                                     OS_NUM,  KC_SPC,  KC_BSPC, _______, NNB_SPC, OS_RTHB
 #endif
